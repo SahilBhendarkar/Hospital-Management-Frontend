f79e9b97b3d7e80bf058c1b51269774e8d850b84
Sahil Bhendarkar <165172115+SahilBhendarkar@users.noreply.github.com>
2026-01-09 18:55:29 +0530

    Update Login.tsx

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,12 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { Eye, EyeOff } from "lucide-react";
 import { motion } from "framer-motion";
 
-const pageVariants = {
-    initial: { opacity: 0, y: 20 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -20 },
-};
-
 const Login: React.FC = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");

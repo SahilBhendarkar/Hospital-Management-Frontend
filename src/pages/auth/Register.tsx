251ccd32ec8264021533c945e52487fe87b66f7e
SahilBhendarkar <sahilbhendarkar19@gmail.com>
2026-01-20 12:51:12 +0530

    removed unnecessary files

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -8,6 +8,7 @@ const Register: React.FC = () => {
             animate={{ opacity: 1, y: 0 }}
             className="min-h-screen flex items-center justify-center"
         >
+            
             <div className="max-w-md w-full bg-white p-10 rounded-3xl shadow-2xl">
                 <h2 className="text-4xl font-bold text-center mb-4">
                     Create Account
